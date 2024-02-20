a0bce4c685fecfcc4c8a9d20954c9d0015a41beb
Jakob Lingel <jlingel68@gmail.com>
2024-02-20 17:54:25 +0100

    Extract text-writer component

--- fe/src/app/components/hero/hero.component.ts
+++ fe/src/app/components/hero/hero.component.ts
@@ -1,71 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TextWriterComponent } from '../text-writer/text-writer.component';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TextWriterComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
 export class HeroComponent {
-  private words = ['Always growing', 'Always improving'];
-  public typeWord: string = '';
-  private displayWord!: Element;
-  //public cursor = '\u{2588}';
-  public cursor = '|';
-  public length = 0;
-
-  ngOnInit() {
-    this.displayWord = document.querySelector('#displayWord')!;
-    setTimeout(() => {
-      this.makeWords();
-    }, 3000);
+  shrink() {
   }
-
-  private async makeWords() {
-    for (let [i, word] of this.words.entries()) {
-      let letters: string[] = word.split('');
-      await this.addLetters(letters);
-      if (i < this.words.length - 1) {
-        await this.removeLetters();
-      }
-    }
-  }
-
-  private addLetters(letters: string[]) {
-    return new Promise((resolve) => {
-      let i = 0;
-      let duration = 100;
-      let interval = setInterval(() => {
-        this.typeWord += letters[i];
-        i++;
-        if (i >= letters.length) {
-          clearInterval(interval);
-          resolve(duration * letters.length);
-        }
-      }, duration);
-    });
-  }
-
-  private removeLetters() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        console.log('remove');
-        let i = 0;
-        let length = this.typeWord.length;
-        this.length = this.typeWord.length - 1;
-        let interval = setInterval(() => {
-          let wordWithPoppedLetter = this.typeWord.slice(0, this.length);
-          this.typeWord = wordWithPoppedLetter;
-          this.length -= 1;
-          i++;
-          if (i >= length) {
-            clearInterval(interval);
-            resolve('blub');
-          }
-        }, 100);
-      }, 2000);
-    });
+  grow() {
   }
 }
---
+++ fe/src/app/components/text-writer/text-writer.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-writer',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-writer.component.html',
+  styleUrl: './text-writer.component.css'
+})
+export class TextWriterComponent {
+  private words = ['Always growing', 'Always improving'];
+  public typeWord: string = '';
+  public cursor = '|';
+  public length = 0;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.makeWords();
+    }, 3000);
+  }
+
+  private async makeWords() {
+    for (let [i, word] of this.words.entries()) {
+      let letters: string[] = word.split('');
+      await this.addLetters(letters);
+      if (i < this.words.length - 1) {
+        await this.removeLetters();
+      }
+    }
+  }
+
+  private addLetters(letters: string[]) {
+    return new Promise((resolve) => {
+      let i = 0;
+      let duration = 100;
+      let interval = setInterval(() => {
+        this.typeWord += letters[i];
+        i++;
+        if (i >= letters.length) {
+          clearInterval(interval);
+          resolve(duration * letters.length);
+        }
+      }, duration);
+    });
+  }
+
+  private removeLetters() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log('remove');
+        let i = 0;
+        let length = this.typeWord.length;
+        this.length = this.typeWord.length - 1;
+        let interval = setInterval(() => {
+          let wordWithPoppedLetter = this.typeWord.slice(0, this.length);
+          this.typeWord = wordWithPoppedLetter;
+          this.length -= 1;
+          i++;
+          if (i >= length) {
+            clearInterval(interval);
+            resolve('blub');
+          }
+        }, 100);
+      }, 2000);
+    });
+  }
+
+}

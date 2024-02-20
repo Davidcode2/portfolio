ecda0b66a1868244b918bf8bc5fb05be6c9a7d5e
Jakob Lingel <jlingel68@gmail.com>
2024-02-21 00:24:58 +0100

    Logos, project component

--- fe/src/app/app.component.ts
+++ fe/src/app/app.component.ts
@@ -4,20 +4,28 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { HeroContainerComponent } from './components/hero-container/hero-container.component';
+import { ProjectComponent } from './components/project/project/project.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroComponent, HeaderComponent, HeroContainerComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeroComponent,
+    HeaderComponent,
+    HeroContainerComponent,
+    ProjectComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   title = 'fe';
 
   ngOnInit() {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
         const square = entry.target.querySelector('.onScroll')!;
 
         if (entry.isIntersecting) {
@@ -33,4 +41,3 @@ export class AppComponent implements OnInit {
     observer.observe(document.querySelector('.slide-wrapper')!);
   }
 }
-

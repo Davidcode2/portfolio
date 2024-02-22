2f7f27870a5547a47c62a23230adf4b9b2afa678
Jakob Lingel <jlingel68@gmail.com>
2024-02-22 18:01:11 +0100

    Github card border

--- fe/src/app/components/project/project/project.component.ts
+++ fe/src/app/components/project/project/project.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { Octokit } from 'octokit';
 
 @Component({
   selector: 'app-project',
@@ -11,8 +13,28 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
   styleUrl: './project.component.css',
 })
 export class ProjectComponent {
-
   ngOnInit() {
+    this.animate();
+  }
+
+  public animateGithubBorder() {
+    var tl = gsap.timeline();
+
+    tl.set('#border-githubButton', { autoAlpha: 20 });
+
+    tl.to('#border-githubButton', { duration: 0.3, borderColor: 'blue' });
+    tl.to('#border-githubButton', { duration: 0.3, borderColor: 'purple' });
+  }
+
+  public animateGithubBorderOut() {
+    var tl = gsap.timeline();
+
+    tl.set('#border-githubButton', { autoAlpha: 20 });
+
+    tl.to('#border-githubButton', { duration: 0.4, borderColor: 'gray' });
+  }
+
+  private animate() {
     gsap.registerPlugin(ScrollTrigger);
     gsap.set('.card', { position: 'relative' });
     let tl = gsap.timeline({
@@ -30,20 +52,36 @@ export class ProjectComponent {
       scale: 1.1,
       rotate: 2,
       stagger: 0.5,
-      filter: 'blur(10px) grayscale(1)',
     });
-    tl.from('.status', {
-      y: 5,
-      opacity: 0,
-      rotate: -20,
-      scale: 0.8,
-      duration: .3,
-    }, "<1");
-    tl.from('.flyInText', {
-      stagger: 0.1,
-      y: 100,
-      opacity: 0,
-      duration: .2,
-    }, "<");
+    tl.to(
+      '.card',
+      {
+        stagger: 0.5,
+        delay: 0.1,
+        filter: 'blur(10px) grayscale(1)',
+      },
+      '<',
+    ),
+      tl.from(
+        '.status',
+        {
+          y: 5,
+          opacity: 0,
+          scale: 0.8,
+          duration: 0.3,
+        },
+        '<1',
+      );
+    tl.from(
+      '.flyInText',
+      {
+        stagger: 0.02,
+        y: 15,
+        filter: 'blur(10px)',
+        opacity: 0,
+        duration: 0.2,
+      },
+      '<',
+    );
   }
 }

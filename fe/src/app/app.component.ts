6a00f4c9dd708cd081fa46cd25f5b1399471d1e8
Jakob Lingel <jlingel68@gmail.com>
2024-02-24 21:42:26 +0100

    Try animating header on x-axis

--- fe/src/app/app.component.ts
+++ fe/src/app/app.component.ts
@@ -8,6 +8,8 @@ import { ProjectComponent } from './components/project/project/project.component
 import { TimelineComponent } from './components/timeline/timeline.component';
 import { SchluesselmomenteProjectComponent } from './components/project/schluesselmomente-project/schluesselmomente-project.component';
 import { KorrekturhilfeProjectComponent } from './components/project/korrekturhilfe-project/korrekturhilfe-project.component';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-root',
@@ -21,48 +23,26 @@ import { KorrekturhilfeProjectComponent } from './components/project/korrekturhi
     ProjectComponent,
     TimelineComponent,
     SchluesselmomenteProjectComponent,
-    KorrekturhilfeProjectComponent
+    KorrekturhilfeProjectComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  title = "Jakob's Corner";
-
   ngOnInit() {
+    this.animateHeader();
   }
 
-  observeProjectSlideStatus() {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        let project = document.querySelector('.project')!;
-
-        if (entry.isIntersecting) {
-          project.classList.add('opacity-100');
-          return; // if we added the class, exit the function
-        }
-
-        // We're not intersecting, so remove the class!
-        project.classList.remove('opacity-100');
-      });
+  private animateHeader() {
+    gsap.registerPlugin(ScrollTrigger);
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#projects',
+        scrub: true,
+      },
     });
-
-    observer.observe(document.querySelector('.project-wrapper')!);
-  }
-
-  observeButtonSlideStatus() {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        const button = entry.target.querySelector('.onScroll')!;
-        if (entry.isIntersecting) {
-          button.classList.add('slide-in');
-          return; // if we added the class, exit the function
-        }
-        // We're not intersecting, so remove the class!
-        button.classList.remove('slide-in');
-      });
+    tl.to('#projects', {
+      xPercent: -100,
     });
-
-    observer.observe(document.querySelector('.slide-wrapper')!);
   }
 }
